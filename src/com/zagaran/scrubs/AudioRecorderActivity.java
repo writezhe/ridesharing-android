8950b2ab2ef06cac8a6ae9785595913d39ea5fc5
zagorsky <josh.zagorsky@gmail.com>
2014-05-28 15:50:17 -0400

    Adds a new, empty activity to eventually hold the audio recorder

---
+++ src/com/zagaran/scrubs/AudioRecorderActivity.java
@@ -0,0 +1,13 @@
+package com.zagaran.scrubs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioRecorderActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_audio_recorder);
+	}
+}
