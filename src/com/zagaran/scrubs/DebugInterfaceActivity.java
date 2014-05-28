eb1f07dc0cb0c68c9d45a0dbbd5b0db188773d64
zagorsky <josh.zagorsky@gmail.com>
2014-05-28 16:03:41 -0400

    Adds buttons to Print and Clear Log. Also removes fragment layout from DebugInterfaceActivity.

--- src/com/zagaran/scrubs/DebugInterfaceActivity.java
+++ src/com/zagaran/scrubs/DebugInterfaceActivity.java
@@ -1,32 +1,26 @@
 package com.zagaran.scrubs;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-public class DebugInterfaceActivity extends ActionBarActivity {
+public class DebugInterfaceActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_debug_interface);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 		
 		startScreenOnOffListener();
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -35,6 +29,7 @@ public class DebugInterfaceActivity extends ActionBarActivity {
 		return true;
 	}
 
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -47,27 +42,23 @@ public class DebugInterfaceActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_debug_interface,
-					container, false);
-			return rootView;
-		}
-	}
-
+	
 	private void startScreenOnOffListener() {
 		final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
 		filter.addAction(Intent.ACTION_SCREEN_OFF);
 		final BroadcastReceiver mReceiver = new ScreenOnOffListener();
 		registerReceiver(mReceiver, filter);
+	}
+	
+	
+	public void printInternalLog(View view) {
+		Log.i("DebugInterfaceActivity", "'Print Internal Log' button was pressed!");
+		// TODO: make this function write to something (Logcat or the app's screen)
+	}
+
+
+	public void clearInternalLog(View view) {
+		Log.i("DebugInterfaceActivity", "'Clear Internal Log' button was pressed!");
+		// TODO: make this function delete the log file
 	}
 }
