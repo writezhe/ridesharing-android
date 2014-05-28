5c8317a667dc9dfc8838d55daaeeb90f2b30dd1a
zagorsky <josh.zagorsky@gmail.com>
2014-05-27 21:01:16 -0400

    Adds listener for screen on/off, power on/off etc. Needs testing!

--- src/com/zagaran/scrubs/DebugInterfaceActivity.java
+++ src/com/zagaran/scrubs/DebugInterfaceActivity.java
@@ -1,15 +1,16 @@
 package com.zagaran.scrubs;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class DebugInterfaceActivity extends ActionBarActivity {
 
@@ -22,6 +23,8 @@ public class DebugInterfaceActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		startScreenOnOffListener();
 	}
 
 	@Override
@@ -61,4 +64,10 @@ public class DebugInterfaceActivity extends ActionBarActivity {
 		}
 	}
 
+	private void startScreenOnOffListener() {
+		final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+		filter.addAction(Intent.ACTION_SCREEN_OFF);
+		final BroadcastReceiver mReceiver = new ScreenOnOffListener();
+		registerReceiver(mReceiver, filter);
+	}
 }
---
+++ src/com/zagaran/scrubs/ScreenOnOffListener.java
@@ -0,0 +1,41 @@
+package com.zagaran.scrubs;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Screen On/Off Listener
+ * Listens for and records when the screen is turned on or off
+ * @author Josh Zagorsky, May 2014
+ */
+public class ScreenOnOffListener extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		// Screen on/off
+		if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+			// TODO: write to a file to record this
+			Log.i("ScreenOnOffListener", "Screen turned off"); }
+		else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+			Log.i("ScreenOnOffListener", "Screen turned on"); }
+		
+		// TODO: TEST THESE:
+		// Power connected/disconnected
+		else if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+			Log.i("ScreenOnOffListener", "Power connected"); } 
+		else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+			Log.i("ScreenOnOffListener", "Power disconnected"); }
+		
+		// Device turned on/off
+		else if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			Log.i("ScreenOnOffListener", "Device finished booting"); }
+		else if (intent.getAction().equals(Intent.ACTION_SHUTDOWN)) {
+			Log.i("ScreenOnOffListener", "Device is shutting down"); }
+		else if (intent.getAction().equals(Intent.ACTION_REBOOT)) {
+			Log.i("ScreenOnOffListener", "Device is rebooting"); }
+	}
+	
+}
