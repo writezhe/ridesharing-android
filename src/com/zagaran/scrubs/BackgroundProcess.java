37a5ef0f018ef63d769f9a754bf5fea8e4ca2464
Teh Beebles <eli@zagaran.com>
2014-06-01 19:42:32 -0400

    added scaffolding to background process

--- src/com/zagaran/scrubs/BackgroundProcess.java
+++ src/com/zagaran/scrubs/BackgroundProcess.java
@@ -1,5 +1,15 @@
 package com.zagaran.scrubs;
 
-public class BackgroundProcess {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackgroundProcess extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
