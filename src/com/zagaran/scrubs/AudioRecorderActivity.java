fa349a5f88c9764e965fd2f3615b5a027435f098
zagorsky <josh.zagorsky@gmail.com>
2014-06-02 20:23:18 -0400

    Makes audio recorder work with only an XML layout file (replaces the programatically-drawn buttons from the Android Developer website code)

--- src/com/zagaran/scrubs/AudioRecorderActivity.java
+++ src/com/zagaran/scrubs/AudioRecorderActivity.java
@@ -3,14 +3,12 @@ package com.zagaran.scrubs;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 /**
@@ -26,47 +24,65 @@ public class AudioRecorderActivity extends Activity {
     private static final String LOG_TAG = "AudioRecorderActivity";
     private static String mFileName = null;
 
-    private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
     
     private boolean currentlyRecording = false;
     private boolean currentlyPlaying = false;
 
-    private PlayButton mPlayButton = null;
-    private MediaPlayer mPlayer = null;
+    
+    public AudioRecorderActivity() {
+	    mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+    }
 
+    
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+		setContentView(R.layout.activity_audio_recorder);        
+    }
+
+    
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mRecorder != null) {
+        	stopRecording();
+        }
+
+        if (mPlayer != null) {
+        	stopPlaying();
+        }
+    }
+
+    
+    // TODO: add arbitrary recording length limiter, like 2 hours
+    // TODO: add UI text saying "the recording can't be longer than X"
+    
+    // When the user presses the "record" button, toggle (start/stop) recording
     public void buttonRecordPressed(View view) {
     	if (!currentlyRecording) {
-			startRecording();
-		} else {
-			stopRecording();
-		}
+    		startRecording();
+    	}
+    	else {
+    		stopRecording();
+    	}
     }
+
     
+    // When the user presses the "play" button, toggle (start/stop) playback
     public void buttonPlayPressed(View view) {
     	if (!currentlyPlaying) {
 			startPlaying();
-		} else {
+		}
+    	else {
 			stopPlaying();
 		}    	
     }
     
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
 
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
-
+    // Start playing back the recording
     private void startPlaying() {
     	currentlyPlaying = true;
     	Button playButton = (Button) findViewById(R.id.buttonPlay);
@@ -77,11 +93,14 @@ public class AudioRecorderActivity extends Activity {
             mPlayer.setDataSource(mFileName);
             mPlayer.prepare();
             mPlayer.start();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             Log.e(LOG_TAG, "prepare() failed");
         }
     }
 
+    
+    // Stop playing back the recording, and reset the button to "play"
     private void stopPlaying() {
     	currentlyPlaying = false;
     	Button playButton = (Button) findViewById(R.id.buttonPlay);
@@ -91,6 +110,8 @@ public class AudioRecorderActivity extends Activity {
         mPlayer = null;
     }
 
+    
+    // Start recording from the device's microphone
     private void startRecording() {
     	currentlyRecording = true;
     	Button recordingButton = (Button) findViewById(R.id.buttonRecord);
@@ -105,13 +126,16 @@ public class AudioRecorderActivity extends Activity {
 
         try {
             mRecorder.prepare();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             Log.e(LOG_TAG, "prepare() failed");
         }
 
         mRecorder.start();
     }
 
+    
+    // Stop recording, and reset the button to "record"
     private void stopRecording() {
     	currentlyRecording = false;
     	Button recordingButton = (Button) findViewById(R.id.buttonRecord);
@@ -120,105 +144,6 @@ public class AudioRecorderActivity extends Activity {
     	mRecorder.stop();
         mRecorder.release();
         mRecorder = null;
-    }
-
-    public class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    public class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
-    public AudioRecorderActivity() {
-	    mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-    }
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-		setContentView(R.layout.activity_audio_recorder);
-        
-		/*Button oldRecordButton = (Button) findViewById(R.id.buttonRecord);
-        Button oldPlayButton = (Button) findViewById(R.id.buttonPlay);
-        
-        replaceButton(oldRecordButton, new RecordButton(getApplicationContext()));
-        replaceButton(oldPlayButton, new PlayButton(getApplicationContext()));*/
-		
-        /*LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-            new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-            new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                0));
-        setContentView(ll);*/
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
-    }
-    
-    
-    private void replaceButton(Button oldButton, Button newButton) {
-    	ViewGroup parent = (ViewGroup) oldButton.getParent();
-    	int index = parent.indexOfChild(oldButton);
-    	parent.removeView(oldButton);
-    	parent.addView(newButton, index);
     }
     
 }
