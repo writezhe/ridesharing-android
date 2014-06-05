bb9357f4308e03ce1599d53c34b6a70d750ec495
zagorsky <josh.zagorsky@gmail.com>
2014-06-05 19:39:36 -0400

    Receive incoming texts and print them to LogCat

--- src/com/zagaran/scrubs/SMSLogger.java
+++ src/com/zagaran/scrubs/SMSLogger.java
@@ -3,6 +3,9 @@ package com.zagaran.scrubs;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 
 // relevant stackoverflow:
@@ -15,9 +18,28 @@ import android.content.Intent;
 public class SMSLogger extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
-		// TODO Auto-generated method stub
-		
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+			Bundle bundle = intent.getExtras();
+			SmsMessage[] messages = null;
+			String messageFrom;
+			if (bundle != null) {
+				try {
+					Object[] pdus = (Object[]) bundle.get("pdus");
+					messages = new SmsMessage[pdus.length];
+					for (int i = 0; i < pdus.length; i++) {
+						messages[i] = SmsMessage.createFromPdu( (byte[]) pdus[i]);
+						messageFrom = messages[i].getOriginatingAddress();
+						String messageBody = messages[i].getMessageBody();
+						Log.i("SMSLogger", "Message from: " + messageFrom);
+						Log.i("SMSLogger", "Message text: " + messageBody);
+					}
+				}
+				catch (Exception e) {
+					Log.i("SMSLogger", "SMS_RECEIVED Caught exception: " + e.getMessage());
+				}
+			}
+		}
 	}
 
 }
