6bb7a44db7105b3b482247f6258f7508afbbab80
Teh Beebles <eli@zagaran.com>
2014-06-13 14:32:02 -0400

    created a simple hashing function, takes a string, returns a string of a hash.  May not work due to an incorrect parameter (the hash function may be named incorrectly.

--- src/com/zagaran/scrubs/EncryptionEngine.java
+++ src/com/zagaran/scrubs/EncryptionEngine.java
@@ -1,5 +1,14 @@
 package com.zagaran.scrubs;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class EncryptionEngine {
-
+	
+	public String hash (String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest steve = MessageDigest.getInstance("SHA-256");
+		steve.update( input.getBytes("UTF-8") );
+		return new String( steve.digest(), "UTF-8") ;
+	}
+	
 }
