78252e22af90a922f482052624885d74e5cec932
zagorsky <josh.zagorsky@gmail.com>
2014-07-02 11:52:18 -0400

    Adds Survey question download functions- not yet used

--- src/org/beiwe/app/survey/QuestionsDownloader.java
+++ src/org/beiwe/app/survey/QuestionsDownloader.java
@@ -1,6 +1,9 @@
 package org.beiwe.app.survey;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,7 +11,12 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.apache.http.client.methods.HttpGet;
 import org.beiwe.app.R;
 
 import android.content.Context;
@@ -56,6 +64,49 @@ public class QuestionsDownloader {
 			e.printStackTrace();
 			return "";
 		}
+	}
+
+	
+	/**
+	 * Get the current survey questions from the server as a JSON file
+	 * @throws Exception
+	 */
+	private void updateLocations() throws Exception {
+	    // Run the HTTP GET on a separate, non-blocking thread
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		Callable<HttpGet> thread = new Callable<HttpGet>() {
+			@Override
+			public HttpGet call() throws Exception {
+				// Copy the survey questions from the server
+				copyFileFromServerToLocal();
+				return null;
+			}
+		};
+		executor.submit(thread);
+	}
+
+	
+	/**
+	 * Read a JSON file from the server, and copy it to a file in the local Android filesystem
+	 * @throws Exception
+	 */
+	private void copyFileFromServerToLocal() throws Exception {
+		URL locationsFileURL;
+		locationsFileURL = new URL(appContext.getResources().getString(R.string.survey_questions_url));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(locationsFileURL.openStream()));
+		
+		// TODO: reconcile this with Eli's filesystem manager
+		String filePath = appContext.getFilesDir() + "/survey1.json";
+		File writeFile = new File(filePath);
+ 		BufferedWriter writer = new BufferedWriter(new FileWriter(writeFile));
+		
+		String inputLine;
+		while ((inputLine = reader.readLine()) != null) {
+			writer.write(inputLine);
+		}
+		
+		reader.close();
+		writer.close();		
 	}
 
 }
