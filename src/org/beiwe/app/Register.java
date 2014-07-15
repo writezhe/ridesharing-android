dbaa1680ce20590d0ee6b986a0e1fd884bc3669e
Teh Beebles <eli@zagaran.com>
2014-07-14 20:51:29 -0400

    added a new Register File with a comment.

---
+++ src/org/beiwe/app/Register.java
@@ -0,0 +1,18 @@
+package org.beiwe.app;
+
+import android.util.Log;
+
+//IMEI: phone SIM identifier.
+//android uuid
+
+//needs to grab data on first device run, and use that to construct a unique identifying  blob to send to the server.
+//this blob will uniquely identify the advice, and assist in pushing the user to the correct actions if they
+//have a new phone, if they have a new phone number, etc.  all identifying data must be hashed.
+//this blob may need to be queryable from the server, it should be unique to each user.
+
+public class Register {
+
+	public Register() {
+		Log.i("","");
+	}
+}
