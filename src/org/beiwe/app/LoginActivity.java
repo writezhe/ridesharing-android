0135009248296944ea80ec6475bdfb1151c04e71
Dor Samet <dori.samet@gmail.com>
2014-07-16 14:36:32 -0400

    Committed the Loading activity, and all changes made accordingly. Loading activity CAN be later changed to have splash screen effects, and such.

--- src/org/beiwe/app/LoginActivity.java
+++ src/org/beiwe/app/LoginActivity.java
@@ -1,13 +1,12 @@
 package org.beiwe.app;
 
-import java.util.HashMap;
-
 import org.beiwe.app.survey.TextFieldKeyboard;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -31,10 +30,7 @@ public class LoginActivity extends Activity {
 	@Override
 	
 	/**
-	 * onCreate method. Logic that goes behind this method -
-	 * IF the session is logged in (AKA shared preferences hold values) - keep the session logged in.
-	 * ELSE The session is not logged in and we should wait for user input.
-	 * Waiting has its own logic that WILL BE CHANGED because we need to hash user-password combos.
+	 * onCreate method. Nothing interesting happens here :)
 	 * 
 	 */
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,33 +48,52 @@ public class LoginActivity extends Activity {
 		textFieldKeyboard.makeKeyboardBehave(passWord);
 	}
 	
-	
+
+	/**
+	 * Logic that goes behind this method -
+	 * IF the session is logged in (AKA shared preferences hold values) - keep the session logged in.
+	 * ELSE The session is not logged in and we should wait for user input.
+	 * 
+	 * Notice there is a direct access to SharedPreferences.
+	 * @param view
+	 */
 	public void loginSequence(View view) {
 		if (session.isLoggedIn()) {
 			Log.i("LoginActivity", "" + session.isLoggedIn());
 			startActivity(new Intent(appContext, DebugInterfaceActivity.class));
+			finish();
 		} else {
 			// TODO: Hashing mechanism goes here for username password combination
 			String username = userName.getText().toString();
 			String password = passWord.getText().toString();
 			
-			HashMap details = session.getUserDetails();
+			// TODO: Ask Eli if there is a safer solution for this.
+			SharedPreferences pref = getSharedPreferences("BeiwePref", 0);
+			String prefUsername = pref.getString(SessionManager.KEY_NAME, "NIL");
+			String prefPassword = pref.getString(SessionManager.KEY_PASSWORD, "NILPASS");
+			Log.i("LoginActivity", prefUsername);
+			Log.i("LoginActivity", prefPassword);
 			
+			// Logic begins here
 			if(username.trim().length() > 0 && password.trim().length() > 0){
-				if(!details.containsKey(username)) {
-					
+				if(!username.equals(prefUsername)) {
+					Toast.makeText(appContext, "Invalid username", 5).show();
 				}
-				if(username.equals("test") && password.equals("test")){ // Needs to be hashed
+				else if(password.equals(prefPassword)){ 
 					session.createLoginSession(username, password);
 					startActivity(new Intent(appContext, DebugInterfaceActivity.class));
 					finish();
-				} 
+				} // TODO: Fragmenting errors/Alerts go here
 				else {Toast.makeText(appContext, appContext.getResources().getString(R.string.incorrect_user_pass_combo), 5).show();} 
 			} 
 			else {Toast.makeText(appContext, "Login Failed", 5).show();} // In case login completely fails
 		}
 	}
 	
+	/**
+	 * Switch to the forgot password screen, without losing the saved state.
+	 * @param view
+	 */
 	public void forgotPassword(View view) {
 		startActivity(new Intent(appContext, ForgotPassword.class));
 	}
--- src/org/beiwe/app/RegisterActivity.java
+++ src/org/beiwe/app/RegisterActivity.java
@@ -1,34 +1,93 @@
 package org.beiwe.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends ActionBarActivity {
 
+/**
+ * Activity used to log a user in to the application for the first time. This activity should only be called on ONCE,
+ * as once the user is logged in, data is saved on the phone.
+ * @author Dori Samet
+ *
+ */
+@SuppressLint("ShowToast")
+public class RegisterActivity extends Activity {
+	
+	private Context appContext;
+	private EditText username;
+	private EditText password;
+	private EditText passwordRepeat;
+	private SessionManager session;
+
+	/**
+	 * Users will go into this activity first to register information on the phone and on the server.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		
+		// onCreate set variables
+		appContext = getApplicationContext();
+		username = (EditText) findViewById(R.id.username_box);
+		password = (EditText) findViewById(R.id.password_box);
+		passwordRepeat = (EditText) findViewById(R.id.repeat_password_box);
+		session = new SessionManager(appContext);
 	}
+	
+	/**
+	 * Registration sequence begins here when the submit button is pressed.
+	 * Normally there would be interaction with the server, in order to verify the user ID as well as the phone ID.
+	 * Right now it does simple checks to see that the user actually inserted a value
+	 * @param view
+	 */
+	@SuppressLint("ShowToast")
+	public void registrationSequence(View view) {
+		String usernameStr = username.getText().toString();
+		String passwordStr = password.getText().toString();
+		String passwordRepeatStr = passwordRepeat.getText().toString();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.register, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		// Logic gauntlet begins here
+		// TODO: For each Toast, create a fragment that displays error message
+		if(usernameStr.length() == 0) {
+			Toast.makeText(appContext, "Invalid username", 5).show();
+			showAlert("Invalid username");
+		} else if (passwordStr.length() == 0) {
+			Toast.makeText(appContext, "Invalid password", 5).show();
+		} else if (passwordRepeatStr.length() == 0 || !passwordRepeatStr.equals(passwordStr)) {
+			Toast.makeText(appContext, "Passwords Mismatch", 5).show();
+		} else {
+			// TODO: Hashing mechanism goes here!!
+			
+			Log.i("RegisterActivity", "Attempting to create a login session");
+			session.createLoginSession(usernameStr, passwordStr);
+			Log.i("RegisterActivity", "Registration complete, attempting to start DebugInterfaceActivity");
+			startActivity(new Intent(appContext, DebugInterfaceActivity.class));
+			finish();
 		}
-		return super.onOptionsItemSelected(item);
+	}
+
+	private void showAlert(String message) {
+		AlertDialog alert = new AlertDialog.Builder(appContext).create();
+		alert.setTitle("Uh-Oh!");
+		alert.setMessage(message);
+		alert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		
+		alert.show();
 	}
 }
