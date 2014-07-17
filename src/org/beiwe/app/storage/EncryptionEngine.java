796f4fb0e78ebbf5d5afd1d6f65d4ad99bc8157f
Teh Beebles <eli@zagaran.com>
2014-07-17 16:33:02 -0400

    comments and spacing in encryption.

--- src/org/beiwe/app/storage/EncryptionEngine.java
+++ src/org/beiwe/app/storage/EncryptionEngine.java
@@ -5,8 +5,12 @@ import java.security.NoSuchAlgorithmException;
 
 public class EncryptionEngine {
 	
+	/** takes a string as input, outputs a hash. 
+	 * @param input A String to hash
+	 * @return a String of the hash result.
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException	 */
 	public String hash (String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		/** takes a string as input, outputs a hash. */
 		MessageDigest hasher = MessageDigest.getInstance("SHA-256"); //make a hasher (verb?) object, include the hash algorithm name.
 		hasher.update( input.getBytes("UTF-8") ); //pass the hash object data (the string passed in)
 		return new String( hasher.digest(), "UTF-8") ; //return the "digest", the computed hash.
