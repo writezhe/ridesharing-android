7a5b20f834597506035fdb3e6f8f003bab128ed5
Dor Samet <dori.samet@gmail.com>
2014-07-18 19:07:01 -0400

    Made a few comment changes to the CallLogger, so I will better understand what to do with it the next time I will look at it.
    I would love it if you guys went through it and saw if it makes sense

--- src/org/beiwe/app/listeners/CallLogger.java
+++ src/org/beiwe/app/listeners/CallLogger.java
@@ -1,5 +1,32 @@
 package org.beiwe.app.listeners;
 
-public class CallLogger {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallLogger extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context appContext, Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	 Uri allCalls = Uri.parse("content://call_log/calls");
+//	 Cursor c = managedQuery(allCalls, null, null, null, null);
+	/*
+	 * TODO: 
+	 * 		Saved variables:
+	 * 		- Last date of logged call
+	 * 			
+	 * 		Flow is this:
+	 * 		1. User ends a call (waiting for a phone state IDLE after a another phone state)
+	 * 		2. Logger runs to check if there are any new calls that should be logged in our file
+	 *		3. Logger searches logs all new entries
+	 *		4. Logger saves the current date as a mark
+	 *		5. Repeat
+	 * 
+	 */
+	
 
 }
