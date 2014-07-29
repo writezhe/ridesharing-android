92b7fb36516ab43de7a096c9eb019154c6a4098c
Teh Beebles <eli@zagaran.com>
2014-07-29 18:20:48 -0400

    tiny refactor

--- src/org/beiwe/app/Timer.java
+++ src/org/beiwe/app/Timer.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class Timer {
 	Context appContext;
-	PendingIntent pIntent;
+	PendingIntent pendingIntent;
 	BroadcastReceiver broReceiver;
 	AlarmManager alarmManager;
 	BackgroundProcess backgroundProcess;
@@ -22,25 +22,25 @@ public class Timer {
 		appContext = backgroundProcess.getApplicationContext();
 	}
 	
+	/*
+	 * Actions in Android are saved as values in locations. For example ACTION_CALL is actually saved as android.intent.action.CALL.
+	 * When using an IntentFilter as an "action filter", we will use this convention, and start the timer using the action 
+	 * "org.beiwe.app.START_TIMER". This will be registered in the background process, and will start the PendingIntent 
+	 * (an intent waiting for something to call it) in the timer.
+	 */
 	public void regsiterBroadcastReceiver(Integer seconds) {
 		seconds *= 1000;
 		broReceiver = broadcastReceiverCreator(seconds);
 		
-		/*
-		 * Actions in Android are saved as values in locations. For example ACTION_CALL is actually saved as android.intent.action.CALL.
-		 * When using an IntentFilter as an "action filter", we will use this convention, and start the timer using the action 
-		 * "org.beiwe.app.START_TIMER". This will be registered in the background process, and will start the PendingIntent 
-		 * (an intent waiting for something to call it) in the timer.
-		 */
 		backgroundProcess.registerReceiver(broReceiver, new IntentFilter("org.beiwe.app.START_TIMER")); 
-		pIntent = PendingIntent.getBroadcast(appContext, 0, new Intent("org.beiwe.app.START_TIMER"), 0);
+		pendingIntent = PendingIntent.getBroadcast(appContext, 0, new Intent("org.beiwe.app.START_TIMER"), 0);
 		
 		// These two lines actually wake up the broadcast receiver to check for incoming intents
-		alarmManager = (AlarmManager)(backgroundProcess.getSystemService(Context.ALARM_SERVICE )); // Make sure to research device is "awake" and "not awake"
 		// http://developer.android.com/reference/android/app/AlarmManager.html#ELAPSED_REALTIME
-		alarmManager.set( (int) SystemClock.elapsedRealtime(), SystemClock.elapsedRealtime() + seconds, pIntent );
+		alarmManager = (AlarmManager)( backgroundProcess.getSystemService(Context.ALARM_SERVICE ));
+		alarmManager.set( (int) SystemClock.elapsedRealtime(), SystemClock.elapsedRealtime() + seconds, pendingIntent );
 	}
-		
+	
 	public BroadcastReceiver broadcastReceiverCreator(final int milliseconds) {		
 		return new BroadcastReceiver() {
 		
@@ -48,7 +48,7 @@ public class Timer {
 			public void onReceive(Context context, Intent intent) {
 				Toast.makeText(context, "Receive broadcast, reseting timer...", 5);
 
-				alarmManager.set((int) SystemClock.elapsedRealtime(), SystemClock.elapsedRealtime() + milliseconds, pIntent);
+				alarmManager.set((int) SystemClock.elapsedRealtime(), SystemClock.elapsedRealtime() + milliseconds, pendingIntent);
 				// Double check if SystemClock.elapsedRealtime works if the phone is asleep
 				// Phone is asleep if there is no app holding it active, ergo, if the backgroundservice is active, 
 				// phone is NOT asleep (even if screen is turned off).
