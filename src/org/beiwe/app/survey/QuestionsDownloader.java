faf911eaa7ed3005415bc376b1f824ccf80ade5d
zagorsky <josh.zagorsky@gmail.com>
2014-08-27 18:07:18 -0400

    Removes fileToString function; no longer used because of 712abb4

--- src/org/beiwe/app/survey/QuestionsDownloader.java
+++ src/org/beiwe/app/survey/QuestionsDownloader.java
@@ -136,35 +136,6 @@ public class QuestionsDownloader {
 		TextFileManager.getCurrentQuestionsFile().write(string);
 	}
 
-		
-	/**
-	 * Read a file (really an InputStream) and return a string
-	 * @param inputStream the file you want to read
-	 * @return String that contains the contents of the file
-	 */
-	private String fileToString(InputStream inputStream) {
-		try {
-			Writer writer = new StringWriter();
-			char[] buffer = new char[1024];
-		    Reader reader;
-			reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-		    int n;
-		    while ((n = reader.read(buffer)) != -1) {
-		        writer.write(buffer, 0, n);
-		    }
-			inputStream.close();
-			return writer.toString();
-		}
-		catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return "";
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
-	
 	
 	/**
 	 * Tells you whether a String is valid JSON
