5fd3e0c3943dfad5f2d2652f3a2d25e10da75128
Dor Samet <dori.samet@gmail.com>
2014-09-15 18:28:00 -0400

    Added the AsyncPostSender file

---
+++ src/org/beiwe/app/ui/AsyncPostSender.java
@@ -0,0 +1,27 @@
+package org.beiwe.app.ui;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.beiwe.app.storage.PostRequestFileUpload;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class AsyncPostSender extends AsyncTask<String, String, String> {
+
+	@Override
+	protected String doInBackground(String... params) {
+		try {
+			int response = PostRequestFileUpload.sendPostRequest(params[0], new URL(params[1]));
+			String strResponse = "" + response;
+			Log.i("AsyncPostSender", "RESPONSE = " + strResponse);
+			return strResponse;
+		} catch (IOException e) {
+			e.printStackTrace();
+			Log.i("AsyncPostSender", "IOError");
+			System.exit(1);
+		}
+		return "Error Occured";
+	}
+}
