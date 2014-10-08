71f212efc20d6316a188f7c74417f49070c5dd9c
Eli Jones <eli@zagaran.com>
2014-10-08 18:57:46 -0400

    cleaned up encryptionengine.

--- src/org/beiwe/app/storage/EncryptionEngine.java
+++ src/org/beiwe/app/storage/EncryptionEngine.java
@@ -24,40 +24,74 @@ import android.util.Log;
 public class EncryptionEngine {
 	
 	private static PublicKey key = null;
-	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	
-	/** takes a string as input, outputs a hash. 
-	 * @param input A String to hash
-	 * @return a UTF-8 String of the hash result.
-	 * @throws NoSuchAlgorithmException
-	 * @throws UnsupportedEncodingException	 */
-	public static String test_hash (String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		/** takes a string as input, outputs a hash. */
-		MessageDigest hasher = MessageDigest.getInstance("SHA-256"); //make a hasher (verb?) object, include the hash algorithm name.
-		hasher.update( input.getBytes("UTF-8") ); //pass the hash object data (the string passed in)
-		return new String( hasher.digest(), "UTF-8") ; //return the "digest", the computed hash.
+	/*############################################################################
+	 * ############################ Phone Numbers ################################
+	 * #########################################################################*/
+	
+	/**Converts a phone number into a 64-character hexadecimal string
+	 * @param phoneNumber
+	 * @return a hexadecimal string, or an error message string */
+	public static String hashPhoneNumber(String phoneNumber) {
+		
+		String standardizedPhoneNumber = standardizePhoneNumber(phoneNumber);
+		
+		// Functionality to test various phone number formats:
+		//		String[] formats = {"1-617-123-4567", "+1-617-123-4567", "16171234567", "6171234567", "+16171234567", "1617-123-4567"};
+		//		Log.i("EncryptionEngine.java", "formats.length = " + formats.length);
+		//		for (int i = 0; i < formats.length; i++) {
+		//			Log.i("EncryptionEngine.java", "String " + formats[i] + " converted to: " + standardizePhoneNumber(formats[i]));
+		//		}
+		
+		try {
+			MessageDigest hasher = MessageDigest.getInstance("SHA-256"); //make a hasher (verb?) object, include the hash algorithm name.
+			hasher.update(standardizedPhoneNumber.getBytes("UTF-8")); //pass the hash object data (the string passed in)
+			return toBase64( hasher.digest() );
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return "Phone number hashing failed";
+		}
 	}
+	
+	/**Put the phone number in a standardized format, so that, for example,
+	 * 2345678901 and +1-234-567-8901 have the same hash
+	 * @param rawNumber the not-yet-standardized number
+	 * @return the hopefully standardized number */
+	private static String standardizePhoneNumber(String rawNumber) {
+		// TODO: Eli/Josh. check many cases, and see if this works for non-US phone numbers.
+		// explore Eli's idea of just grabbing the last 10 numeric digits and using those.
+		// If it doesn't, make declaration about false negative phone number matches.
+		String formattedNumber = PhoneNumberUtils.formatNumber(rawNumber);
+
+		if (formattedNumber.startsWith("+1-"))     { return formattedNumber; }
+		else if (formattedNumber.startsWith("1-")) { return "+1-" + formattedNumber.substring(2); }
+		else { return "+1-" + formattedNumber; }
+	}
+	
+	/*############################################################################
+	 * ############################### Hashing ###################################
+	 * #########################################################################*/
 	
 	// TODO: Eli. Work out why this returns strings of varying length, they should definitely all be the same.
 	/** Takes a string as input, handles the usual thrown exceptions, and return a hash string of that input.  
 	 * @param input A String to hash
 	 * @return a Base64 String of the hash result. */
 	public static String safeHash (String input) {
-		/** */
 		try {
-			return unsafeHash( input );
-		} catch (NoSuchAlgorithmException e) {
+			return unsafeHash( input ); }
+		catch (NoSuchAlgorithmException e) {
 			Log.e("Hashing function", "NoSuchAlgorithmException");
-			System.exit(1);
 			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
+			System.exit(1); }
+		catch (UnsupportedEncodingException e) {
 			Log.e("Hashing function", "UnsupportedEncodingException");
 			e.printStackTrace();
-			System.exit(2);
-		}
+			System.exit(2); }
 		Log.e("hash", "this line of code should absolutely never run.");
 		return null;
 	}
+	
 	
 	/** Takes a string as input, outputs a hash.
 	 * @param input A String to hash.
@@ -70,69 +104,13 @@ public class EncryptionEngine {
 		hash = MessageDigest.getInstance("SHA-256");
 		hash.update( input.getBytes("UTF-8") );
 		return_data = hash.digest();
-		return Base64.encodeToString(return_data, Base64.NO_WRAP | Base64.NO_PADDING);		
-	}
-	
-	/**Converts a phone number into a 64-character hexadecimal string
-	 * @param phoneNumber
-	 * @return a hexadecimal string, or an error message string */
-	public static String hashPhoneNumber(String phoneNumber) {
-		
-		String standardizedPhoneNumber = standardizePhoneNumber(phoneNumber);
-	
-		// Functionality to test various phone number formats:
-		//		String[] formats = {"1-617-123-4567", "+1-617-123-4567", "16171234567", "6171234567", "+16171234567", "1617-123-4567"};
-		//		Log.i("EncryptionEngine.java", "formats.length = " + formats.length);
-		//		for (int i = 0; i < formats.length; i++) {
-		//			Log.i("EncryptionEngine.java", "String " + formats[i] + " converted to: " + standardizePhoneNumber(formats[i]));
-		//		}
-		
-		try {
-			MessageDigest hasher = MessageDigest.getInstance("SHA-256"); //make a hasher (verb?) object, include the hash algorithm name.
-			hasher.update(standardizedPhoneNumber.getBytes("UTF-8")); //pass the hash object data (the string passed in)
-//			return bytesToHex(hasher.digest());
-			return Base64.encodeToString(hasher.digest(), Base64.NO_WRAP | Base64.NO_PADDING);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return "Phone number hashing failed";
-		}
+		return toBase64(return_data);		
 	}
 	
 	
-	/**Put the phone number in a standardized format, so that, for example,
-	 * 2345678901 and +1-234-567-8901 have the same hash
-	 * @param rawNumber the not-yet-standardized number
-	 * @return the hopefully standardized number */
-	private static String standardizePhoneNumber(String rawNumber) {
-		// TODO: Eli/Josh. check many cases, and see if this works for non-US phone numbers.
-		// explore Eli's idea of just grabbing the last 10 numeric digits and using those.
-		// If it doesn't, make declaration about false negative phone number matches.
-		String formattedNumber = PhoneNumberUtils.formatNumber(rawNumber);
-
-		if (formattedNumber.startsWith("+1-")) {
-			return formattedNumber;
-		} else if (formattedNumber.startsWith("1-")) {
-			return "+1-" + formattedNumber.substring(2);			
-		} else {
-			return "+1-" + formattedNumber;
-		}
-	}
-	
-	
-//	/**Converts a byteArray into a hexadecimal string.
-//	 * Based heavily on: http://stackoverflow.com/a/9855338
-//	 * @param byteArray
-//	 * @return a String composed only of characters 0-9 and A-F */
-//	private static String bytesToHex(byte[] byteArray) {
-//		char[] hexCharArray = new char[byteArray.length * 2];
-//		for (int i = 0; i < byteArray.length; i++) {
-//			int value = byteArray[i] & 0xFF;
-//			hexCharArray[i * 2] = hexArray[value >>> 4];
-//			hexCharArray[i * 2 + 1] = hexArray[value & 0x0F];
-//		}
-//		return new String(hexCharArray);
-//	}
+	/*############################################################################
+	 * ############################ Encryption ###################################
+	 * #########################################################################*/
 	
 	
 	/**Encrypts data using the RSA cipher and the public half of an RSA key pairing provided by the server. 
@@ -156,10 +134,9 @@ public class EncryptionEngine {
 		catch (IllegalBlockSizeException e1) { Log.e("Encryption Engine", "The key is malformed."); } 
 		catch (BadPaddingException e2) { Log.e("Encryption Engine", "Something went wrong, go research Padding Exceptions. (BadPaddingException)"); }
 		
-		//This is a bitwise or statement.
-		return Base64.encodeToString(encryptedText, Base64.NO_WRAP | Base64.NO_PADDING);
-//		return  bytesToHex( encryptedText );
+		return toBase64(encryptedText);
 	}
+
 	
 	/**Looks for the public key file and imports it.
 	 * Spews out human readable errors to the Log if something seems wrong. */
@@ -170,12 +147,14 @@ public class EncryptionEngine {
 		
 		try {
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			key = keyFactory.generatePublic( x509EncodedKey );
-		} catch (NoSuchAlgorithmException e1) {
+			key = keyFactory.generatePublic( x509EncodedKey ); }
+		catch (NoSuchAlgorithmException e1) {
 			Log.e("Encryption Engine", "ENCRYPTION HAS FAILED BECAUSE RSA IS NOT SUPPORTED?  AN ENCRYPT OPERATION IS ABOUT TO FAIL.");
-			e1.printStackTrace();
-		} catch (InvalidKeySpecException e2) {
-			Log.e("Encryption Engine", "The provided RSA public key is NOT VALID.\nBEFORE doing anything else check that the key is the PUBLIC half of an RSA key pairing." );
-		}
+			e1.printStackTrace(); }
+		catch (InvalidKeySpecException e2) {
+			Log.e("Encryption Engine", "The provided RSA public key is NOT VALID." ); }
 	}
+	
+	
+	private static String toBase64(byte[] data) {return Base64.encodeToString(data, Base64.NO_WRAP | Base64.NO_PADDING); }
 }
