f0f992e4463319013af93faee4e736b51ae36ae7
Eli Jones <eli@zagaran.com>
2014-10-27 22:19:06 -0400

    Almost totally rewrote resetpassword activity.  Uses HTTPAsync.  Does the correct password hashing. No more... weird logic trees.
    Something is broken in the ResetPasswordActivity's xml definition, this causes the HTTPAsync classe to
    freak out when it is used.

--- src/org/beiwe/app/ui/ResetPasswordActivity.java
+++ src/org/beiwe/app/ui/ResetPasswordActivity.java
@@ -4,9 +4,11 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
+import org.beiwe.app.DebugInterfaceActivity;
 import org.beiwe.app.R;
 import org.beiwe.app.networking.AsyncPostSender;
 import org.beiwe.app.networking.HTTPAsync;
+import org.beiwe.app.networking.PostRequest;
 import org.beiwe.app.session.LoginManager;
 import org.beiwe.app.storage.EncryptionEngine;
 import org.beiwe.app.survey.TextFieldKeyboard;
@@ -14,6 +16,7 @@ import org.beiwe.app.survey.TextFieldKeyboard;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,9 +38,7 @@ public class ResetPasswordActivity extends Activity {
 	private EditText newPassword;
 	private EditText newPasswordRepeat;
 	
-	/**
-	 * onCreate method. Nothing interesting happens here...
-	 */
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,43 +56,75 @@ public class ResetPasswordActivity extends Activity {
 		textFieldKeyboard.makeKeyboardBehave(newPassword);
 		textFieldKeyboard.makeKeyboardBehave(newPasswordRepeat);
 	}
+	
+	
+	
+	//TODO: Eli. update this doc
 	/** Each time there is an error, such like an incorrect username, the program will throw an alert,
 	 *  informing the user of the error.
 	 *  
 	 *  If the user succeeds in logging in, the activity finishes.
 	 * @param view
 	 * @throws UnsupportedEncodingException 
-	 * @throws NoSuchAlgorithmException 
-	 */
+	 * @throws NoSuchAlgorithmException */
 	public void resetPasswordSequence(View view) {
 		// Old password, and old password hashed
 		String oldPassStr = oldPass.getText().toString();
-		String oldPassStrHash = EncryptionEngine.safeHash(oldPassStr);
 		
 		// New password that will be pushed to the server
 		String newPasswordStr = newPassword.getText().toString();
 		String newPasswordRepeatStr = newPasswordRepeat.getText().toString();
 
-		// Cases: username mismatch, userID mismatch, passwords mismatch, and repeat password with actual password mismatch. 
-		//TODO: Eli. this logic is used in at least... 3 place: here ,login, and creation.  Modularize it.
-		if(oldPassStr.length() == 0 || !oldPassStrHash.equals( LoginManager.getPassword() ) ) {
-			AlertsManager.showAlert(appContext.getResources().getString(R.string.invalid_old_password), this); }
-		else if (newPasswordStr.length() == 0) { // TODO: MAKE LENGTH CHECK CORRECT.
-			AlertsManager.showAlert(appContext.getResources().getString(R.string.invalid_password), this); }
-		else if (!newPasswordRepeatStr.equals(newPasswordStr)) {
-			AlertsManager.showAlert(appContext.getResources().getString(R.string.password_mismatch), this); }
-		else { makeResetPasswordThread(newPasswordStr); }
-	}
-	
-	
-	/** Makes an Async Thread to send with the new Password*/
-	private void makeResetPasswordThread(String newPassword) {
-//		new AsyncPostSender("http://beiwe.org/set_password", this, newPassword).execute();
-		doResetPassword("http://beiwe.org/set_password");
-	}
-	
-	private void doResetPassword(String url) { new HTTPAsync(url, this) {
+		// Cases: passwords mismatch, and repeat password with actual password mismatch.
+		//make sure the old password matches, then...
+		if ( LoginManager.checkPassword( oldPassStr ) ) {
+			
+			//check that both inputs are identical
+			if ( !newPasswordRepeatStr.equals(newPasswordStr) ) {
+				AlertsManager.showAlert(appContext.getResources().getString(R.string.password_mismatch), this);
+				return;
+			}
+			//check that the proposed password is a valid password
+			if ( LoginManager.validatePassword(newPasswordStr, this) ){
+				Log.i("debugging", "validated password");
+				doResetPassword("http://beiwe.org/set_password", newPasswordStr);
+				//note: the reset password function expects a plaintext, unhashed password.
+			}
+			return;
+		}
+		AlertsManager.showAlert(appContext.getResources().getString(R.string.invalid_old_password), this);
 		
+				
+	}
+	
+	
+	private void doResetPassword(String url, final String newPassword) { new HTTPAsync(url, this) {
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			parameters = PostRequest.makeParameter( "new_password", newPassword );
+			Log.i("debugging", "about to send post request");
+			response = PostRequest.asyncPostHandler( parameters, url );
+			return null; //haaaate
+		}
+		
+		//FIXME: Josh.  Help!  there is something going on here, I think it involves Activity IDs, a subject I am ignorant on.
+		// There is a 'Spinner' object that is defined in the HTTPAsync class, it is instantiated using this ResetPasswordActivity
+		// when the HTTPAsync object is instantiated/executed.
+		// Setting the spinner to visible breaks, but only with the ResetPasswordActivity, it works fine
+		// when the same behavior occurs with the RegisterActivity.
+		// (Otherwise, the logic here works and the password is successfully reset on both the server and the devic)
+		
+		@Override
+		protected void onPostExecute(Void arg) {
+			if (response == 200) { 
+				LoginManager.setPassword(newPassword);
+				//TODO: Eli/Josh.  we need a "successfully changed password alert.
+				//TODO: postproduction. Change to point at regular activity.
+				activity.startActivity(new Intent(activity.getApplicationContext(), DebugInterfaceActivity.class));
+				activity.finish();
+			}
+			super.onPostExecute(arg);
+		}
 	};}
 	
 	
