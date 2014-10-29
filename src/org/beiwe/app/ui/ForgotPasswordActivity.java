50ac63bdb64c7a0ccd05c839d051994e8453e744
Eli Jones <eli@zagaran.com>
2014-10-28 22:58:02 -0400

    failed to save before previous commit.  Updated the ForgotPassword activity to handle errors better (was missing a case), removed old cruft.

--- src/org/beiwe/app/ui/ForgotPasswordActivity.java
+++ src/org/beiwe/app/ui/ForgotPasswordActivity.java
@@ -21,19 +21,12 @@ import android.widget.EditText;
  */
 public class ForgotPasswordActivity extends Activity {
 
-	private Context appContext;
-
-	private EditText newPassword;
-	private EditText newPasswordRepeat;
 	private String hashedNewPassword;
 //	private String oldPasswordhash; // = LoginSessionManager.getPassword();
 	
 	//TODO: Josh. (and maybe Eli, but I am working on other logic stuff sooo...
-	// This activity should display the user's id, it should probably also only have a single input for the reset key provided by the survey administrators.
-	// On a successful password reset it should immediately send the user to the make a new password activity.
+	// This activity should display a single button.  On a successful network operation the password is set to the new value.
 	// The make new password activity should probably have a button that says return to app.
-	// Though it is unlikely, this gets very complicated if a user gets a notification for a survey/recording, 
-	//   fails to log in, does a password reset without ever leaving the app, resets their password, and then expects to take that survey...
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,22 +35,18 @@ public class ForgotPasswordActivity extends Activity {
 
 		//TODO: Josh/Eli. make some letters that say "your user ID is blah, press this button to ... query the server for a new password" or something
 		
-		// Variable assignment
-		appContext = getApplicationContext();
-		newPassword = (EditText) findViewById(R.id.forgot_password_enter_password);
-		newPasswordRepeat = (EditText) findViewById(R.id.forgot_password_enter_password_repeat);
 	}
 	
 	/** calls the reset password HTTPAsync query. */
 	private void registerNewPassword(View view) {
-		async_thing("http://beiwe.org/forgot_password");
+		doForgotPasswordRequest("http://beiwe.org/forgot_password");
 	}
 	
 	//  ######  Stub code, working on the new SimpleAsync class.  #### 
 	
 	/**Creates an SimpleAsync to make a HTTP Post Request  
 	 * @param url */
-	private void async_thing(final String url) { new HTTPAsync(url, this) {  //This is a retarded spacing hack...
+	private void doForgotPasswordRequest(final String url) { new HTTPAsync(url, this) {  //This is a retarded spacing hack...
 
 		@Override
 		protected Void doInBackground(Void... arg0) {
@@ -76,8 +65,7 @@ public class ForgotPasswordActivity extends Activity {
 			super.onPostExecute(arg);
 			Log.i("ForgotPasswordActivity", "old password hash: " + LoginManager.getPassword() + " new password(unhashed): " + responseString);
 			
-			
-			if ( responseString == null ) { return; } //this is the case where the network request has failed.
+			if ( responseString == null || response != -1 ) { return; } //this is the case where the network request has failed.
 			
 			LoginManager.setPassword(responseString);
 			
