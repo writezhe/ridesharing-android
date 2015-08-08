afabdbfde2c9a4893d7681c9755235516eb21354
Eli Jones <eli@zagaran.com>
2015-08-07 21:17:02 -0400

    I think that scheduling is actually finished and functional.

--- src/org/beiwe/app/survey/SurveyScheduler.java
+++ src/org/beiwe/app/survey/SurveyScheduler.java
@@ -2,6 +2,7 @@ package org.beiwe.app.survey;
 
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import org.beiwe.app.BackgroundService;
@@ -19,40 +20,35 @@ import java.util.ArrayList;
 public class SurveyScheduler {
 
 	public static void scheduleSurvey(String surveyId) {
-		
-		String timeString = PersistentData.getSurveyTimes(surveyId);
+		int today;
 		JSONArray JSONTimes;
-		try { JSONTimes = new JSONArray(timeString); }
-		catch (JSONException e) {
-			e.printStackTrace();
-			throw new NullPointerException(e.getMessage());
-		}
-		
-		int today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-		List<String> days = JSONUtils.jsonArrayToStringList(JSONTimes);
-
-		//create a list of days of the week, with order as such:
-		// 0: today
-		// 1: tomorrow
-		// 2: day after ...
-
 		JSONArray dayJSON;
 		ArrayList<Integer> dayInts;
 		ArrayList<ArrayList<Integer>> timesList = new ArrayList<ArrayList<Integer>>(7);
-
+		String timeString = PersistentData.getSurveyTimes(surveyId);
+		
+		Calendar thisDay = Calendar.getInstance();
+		//turns out the first day of the week is not necessarily Sunday. So, in case the user is in such a Locale we manually set that.
+		thisDay.setFirstDayOfWeek(Calendar.SUNDAY);
+		today = thisDay.get(Calendar.DAY_OF_WEEK) - 1;
+		
+		try { JSONTimes = new JSONArray(timeString); }
+		catch (JSONException e) { e.printStackTrace(); //If this fails we have significant problems, but probably the errors come from external factors.
+			throw new NullPointerException(e.getMessage()); }
+		List<String> days = JSONUtils.jsonArrayToStringList(JSONTimes);
+		
+		//we will now create a list of days of the week, with order like this:
+		// 0: today, 1: tomorrow, 2: day after ...
 		for (int i=0; i <= 6; i++) {
 			try { dayJSON = new JSONArray(days.get(i)); }  //convert to json array
-			catch (JSONException e) {
-				e.printStackTrace();
-				throw new NullPointerException(e.getMessage());
-			}
+			catch (JSONException e) { e.printStackTrace(); //Again, if this crashes we have problems but they probably come from external factors.
+				throw new NullPointerException(e.getMessage()); }
 			dayInts = JSONUtils.jsonArrayToIntegerList(dayJSON); //convert to (iteraable) list of ints
-			Collections.sort(dayInts); //ensure sorted
-			if (i < today) timesList.add( dayInts ); //if index < today, add to end
-			if (i == today) timesList.add(0, dayInts ); //if index is today, add to beginning
-			if (i > today) timesList.add(1, dayInts ); //if index > today, insert at position [1] 
+			Collections.sort(dayInts); //ensure sorted because... because.
+			if (i < today) { timesList.add( dayInts ); } //if index < today, add to end
+			if (i == today) { timesList.add(0, dayInts ); } //if index is today, add to beginning
+			if (i > today) { timesList.add(1, dayInts ); } //if index > today, insert at position [1]
 		}
-		Log.i("survey scheduler", "timings list: " + timesList);
 		
 //		we now have a double nested list of lists.  element 0 is today, element 1 is tomorrow
 		// the inner list contains the times of day at which the survey should trigger, these times are values between 0 and 86,400
@@ -62,37 +58,34 @@ public class SurveyScheduler {
 		Calendar newAlarmTime = findNextAlarmTime(timesList);
 		if (newAlarmTime == null) {
 			Log.w("SurveyScheduler", "there were no times at all in the provided timings list.");
-			return; } 
+			return; }
 		BackgroundService.setSurveyAlarm(surveyId, newAlarmTime);
 	}
 	
 	private static Calendar findNextAlarmTime( ArrayList<ArrayList<Integer>> timesList) {
 		Calendar now = Calendar.getInstance();
-		Calendar startOfDayTime = getTimeFromStartOfDayOffset(0);		
-		
+//		Log.d("scheduler", "now: " + now);
 		Calendar possibleAlarmTime = null;
 		Calendar firstPossibleAlarmTime = null;
 		Boolean firstFlag = true;
-
+		int days = 0;
 		for ( ArrayList<Integer> day : timesList ) { //iterate through the days of the week
 			for (int time : day) { //iterate through the times in each day
 				if (time > 86400 || time < 0) { throw new NullPointerException("time parser received an invalid value in the time parsing: " + time); }
 				possibleAlarmTime = getTimeFromStartOfDayOffset(time);
-				
 				if (firstFlag) { //grab the first time we come across in case it falls into the edge case.
 					firstPossibleAlarmTime = (Calendar) possibleAlarmTime.clone();
-					firstFlag = false; }
-				
+					firstFlag = false;
+				}
+				possibleAlarmTime.add(Calendar.DATE, days); //add to this time the appropriate number of days
 				if ( possibleAlarmTime.after( now ) ) { //If the time is in the future, return that time.
-					Log.d("Scheduler", "checked, yup: " + possibleAlarmTime );
-					return possibleAlarmTime; }
-				Log.d("Scheduler", "checked, nope: " + possibleAlarmTime);
+//					Log.d("Scheduler", "checked, yup: " + possibleAlarmTime );
+					return possibleAlarmTime;
+				}
+//				Log.d("Scheduler", "checked, nope: " + possibleAlarmTime);
 			}
-			//TODO: Eli/Josh.  This line is based on my fuzzy understanding of the java Calendar class.  Please research and help debug.
-			startOfDayTime.add(Calendar.DATE, 1); //advance our start of day to the next day.
+			days++; //advance to next day...
 		}
-		//handles the case where the next alarm is earlier in the day than right now, 1 week from now.
-		// (i.e. no matches in the range of times that fall after right now occuring in the list(s) of times
 		/* Warning: for some reason... if you try to throw a null pointer exception in here the app freezes. */
 		//TODO: Eli/Josh.  determine why the app stalls when nullpointerexceptions are thrown on... non gui threads?  insert a null pointer exception here and comment out the remainer of the function to see what I am talking about.
 //		throw new NullPointerException("totally arbitrary message");
@@ -103,13 +96,13 @@ public class SurveyScheduler {
 	
 	private static Calendar getTimeFromStartOfDayOffset(int offset) {
 		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.HOUR_OF_DAY, offset / 3600 );//TODO: Eli/Josh. probably don't need to set both of these?
-//		calendar.set(Calendar.HOUR, offset / 3600 ); //seconds divided by seconds per hour yields hour of day
+		calendar.set(Calendar.HOUR_OF_DAY, offset / 3600 ); //seconds divided by seconds per hour yields hour of day
+//		calendar.set(Calendar.HOUR, offset / 3600 ); //this one appears to be derived, so we don't bother setting it.
 		calendar.set(Calendar.MINUTE, offset / 60 % 60); //seconds divided by sixty mod sixty yields minutes
 		calendar.set(Calendar.SECOND, offset % 60); //seconds mod 60 yields seconds into minute
 		calendar.set(Calendar.MILLISECOND, 0);
-		Log.d("scheduler", "time of day  - " + calendar.get(Calendar.HOUR) + ":"  + calendar.get(Calendar.MINUTE)+ ":" + calendar.get(Calendar.SECOND));
-		Log.d("scheduler", "raw t of day - " + offset / 3600 + ":" + offset / 60 % 60 + ":" + offset % 60 );
+//		Log.d("scheduler", "time of day  - " + calendar.get(Calendar.HOUR) + ":"  + calendar.get(Calendar.MINUTE)+ ":" + calendar.get(Calendar.SECOND));
+//		Log.d("scheduler", "raw t of day - " + offset / 3600 + ":" + offset / 60 % 60 + ":" + offset % 60 );
 		return calendar;
 	}
 
