d5b8947a10e88770c74bc4888b348aaf8d66460a
Eli Jones <eli@zagaran.com>
2016-03-15 21:01:47 -0400

    removed unnecessary import

--- src/org/beiwe/app/survey/JsonParser.java
+++ src/org/beiwe/app/survey/JsonParser.java
@@ -2,7 +2,6 @@ package org.beiwe.app.survey;
 
 import org.beiwe.app.JSONUtils;
 import org.beiwe.app.R;
-import org.beiwe.app.storage.PersistentData;
 import org.beiwe.app.ui.TextFieldType;
 import org.beiwe.app.ui.TextFieldType.Type;
 import org.json.JSONArray;
