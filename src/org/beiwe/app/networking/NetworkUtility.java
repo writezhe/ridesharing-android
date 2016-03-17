67939588543f47a89c18ef60b88a042a76dccec3
Eli Jones <eli@zagaran.com>
2016-03-17 18:46:11 -0400

    updated version NetworkUtility with code path for new android 6 support.

--- src/org/beiwe/app/networking/NetworkUtility.java
+++ src/org/beiwe/app/networking/NetworkUtility.java
@@ -2,6 +2,7 @@ package org.beiwe.app.networking;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 
 
@@ -11,9 +12,26 @@ import android.net.NetworkInfo;
 public class NetworkUtility {
 	/**Return TRUE if WiFi is connected; FALSE otherwise
 	 * @return boolean value of whether the wifi is on and network connectivity is available. */
+	@SuppressWarnings("deprecation")
 	public static Boolean getWifiState(Context appContext ) {
 		ConnectivityManager connManager = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		return mWifi.isConnected();
+		NetworkInfo networkInfo;
+		//Android 6 adds support for multiple network connections of the same type, so the get by type command is deprecated.
+		if (android.os.Build.VERSION.SDK_INT < 23) { //do android < 6
+			networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			return networkInfo.isConnected() && networkInfo.isAvailable();
+		} else { //do android >= 6
+			//FIXME: test.
+			Network[] networks = connManager.getAllNetworks();
+			if (networks == null) { return false; } //No network connectivity at all.
+			for (Network network : connManager.getAllNetworks() ) {
+				networkInfo = connManager.getNetworkInfo(network);
+				if ( networkInfo.getType() == ConnectivityManager.TYPE_WIFI && networkInfo.isConnected() && networkInfo.isAvailable()) {
+					return true; //return true if there is a wifi connection.
+				}		
+			}
+			return false;
+		}
 	}
 }
+
