a3576393f7af2a3e2ac46a12e27b3a4015528a4c
Eli Jones <eli@zagaran.com>
2017-02-22 18:14:49 -0500

    updates to wifi scans, the data is better but is partially separated into the debug log.

--- app/src/main/java/org/beiwe/app/listeners/WifiListener.java
+++ app/src/main/java/org/beiwe/app/listeners/WifiListener.java
@@ -8,6 +8,7 @@ import org.beiwe.app.storage.TextFileManager;
 import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 /**WifiListener
  * WifiListener houses a single public function, scanWifi.  This function grabs the mac
@@ -16,13 +17,12 @@ import android.net.wifi.WifiManager;
  * @author Eli */
 public class WifiListener {
 	private static WifiManager wifiManager;
-	
 	public static String header = "hashed MAC, frequency, RSSI";
 	
 	/** WifiListener requires an application context in order to access 
 	 * the devices wifi info.  
 	 * @param appContext requires a Context */
-	private WifiListener (Context appContext){
+	private WifiListener (Context appContext) {
 		wifiManager = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE); }
 	
 	public static void initialize( Context context ) { new WifiListener( context ); } 
@@ -30,24 +30,10 @@ public class WifiListener {
 	//#######################################################################################
 	//#############################  WIFI STATE #############################################
 	//#######################################################################################
-	
-	/** checks the state of the devices wifi, returns True if it is on and able to provide
-	 * us with SSID broadcast data.
-	 * @return boolean of whether we can gather wifi data. */
-	private static boolean checkState() {
-		int state = wifiManager.getWifiState();
-		if (WifiManager.WIFI_MODE_FULL == state ||
-			WifiManager.WIFI_MODE_FULL_HIGH_PERF == state ||
-			WifiManager.WIFI_MODE_SCAN_ONLY == state ||
-			//TODO: Eli. Low priority. Android studio indicates that WifiManager.WIFI_STATE_ENABLED == state can only evaluate as false, investigate
-			WifiManager.WIFI_STATE_ENABLED == state) {
-			return true; }
-		return false;
-	}
-	
+
 	/** Writes to the wifiLog file all mac addresses of local wifi beacons. */
 	public static void scanWifi() {
-		if ( checkState() ) {
+		if ( wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED ) {
 			List<ScanResult> scanResults = wifiManager.getScanResults();
 			if (scanResults != null) {
 				//we save some compute on the encryption here by dumping all the lines to print in one go.
@@ -60,7 +46,10 @@ public class WifiListener {
 				TextFileManager.getWifiLogFile().newFile(); //note: the file name's timestamp is actually relevant, so we always make a new file.
 				TextFileManager.getWifiLogFile().writeEncrypted( data.toString() );
 				TextFileManager.getWifiLogFile().closeFile();
-			}
-		}
+//				Log.d("WIFI", "DONE GONE DID SCAN.");
+			} // and provide a debug log statement for data researchers if we cannot get scan data right now.
+		} else {
+//			Log.d("WIFI", "YAP SKIPPING DUE TO DISABLEMENT.");
+			TextFileManager.getDebugLogFile().writeEncrypted(System.currentTimeMillis() + " wifi is not available for scanning at this time."); }
 	}
 }
