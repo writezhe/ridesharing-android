de728d83555907b8b77c0e88c235774c08895b8f
zagorsky <josh.zagorsky@gmail.com>
2017-05-22 18:23:21 -0400

    Implements demo code for getting FCM token and receiving a message

---
+++ app/src/main/java/org/beiwe/app/networking/FCMInstanceIDService.java
@@ -0,0 +1,19 @@
+package org.beiwe.app.networking;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FCMInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.w("FirebaseInstanceIDServ", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+}
