writezhe/ridesharing-android

commit 291eb6887c3a0dcf15191408e56607c48cac72bd

Submits timezone name with registration API call

app/src/main/java/org/beiwe/app/ui/registration/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.util.TimeZone;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -109,6 +110,7 @@ public class RegisterActivity extends RunningBackgroundServiceActivity {
 						PostRequest.makeParameter("model", DeviceInfo.getModel() ) +
 						PostRequest.makeParameter("product", DeviceInfo.getProduct() ) +
 						PostRequest.makeParameter("fcm_instance_id", PersistentData.getFCMInstanceID() ) +
+						PostRequest.makeParameter("timezone", TimeZone.getDefault().getID() ) +
 						PostRequest.makeParameter("beiwe_version", DeviceInfo.getBeiweVersion() );
 					
 			responseCode = PostRequest.httpRegister(parameters, url);

commit 140f7c070937ddfd9fb11fa39d3fdf8fd69b0288

Fixes TimeZone crash on older devices caused by 291eb68

app/src/main/java/org/beiwe/app/ui/registration/RegisterActivity.java
@@ -110,7 +110,7 @@ public class RegisterActivity extends RunningBackgroundServiceActivity {
 						PostRequest.makeParameter("model", DeviceInfo.getModel() ) +
 						PostRequest.makeParameter("product", DeviceInfo.getProduct() ) +
 						PostRequest.makeParameter("fcm_instance_id", PersistentData.getFCMInstanceID() ) +
-						PostRequest.makeParameter("timezone", TimeZone.getDefault().getID() ) +
+						PostRequest.makeParameter("timezone", getTimezone() ) +
 						PostRequest.makeParameter("beiwe_version", DeviceInfo.getBeiweVersion() );
 					
 			responseCode = PostRequest.httpRegister(parameters, url);
@@ -139,7 +139,15 @@ public class RegisterActivity extends RunningBackgroundServiceActivity {
 		if (phoneNumber == null) { return EncryptionEngine.hashPhoneNumber(""); }
 		return EncryptionEngine.hashPhoneNumber(phoneNumber);
 	}
-	
+
+	/** Return the time zone the phone is currently in **/
+	public String getTimezone() {
+		try {
+			return TimeZone.getDefault().getID();  // Only works in Android API 24+
+		} catch (NoClassDefFoundError e) {
+			return java.util.TimeZone.getDefault().getID();
+		}
+	}
 	
 	/*####################################################################
 	###################### Permission Prompting ##########################