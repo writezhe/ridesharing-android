deab0c7719fe79e24a5e0532cf890950e3ca31c2
zagorsky <josh.zagorsky@gmail.com>
2017-06-30 15:49:00 -0400

    Updates app version to 2.1.5, level 18

--- app/src/main/java/org/beiwe/app/DeviceInfo.java
+++ app/src/main/java/org/beiwe/app/DeviceInfo.java
@@ -41,7 +41,8 @@ public class DeviceInfo {
 	 * 14: app version 2.1.2, minor behavior improvement in extremely rare occurrence inside of sessionActivity, see inline documentation there for details.
 	 * 15: bug fix in crash handler
 	 * 16: app version 2.1.3, rewrite of file uploading to fix App Not Responding (ANR) errors; also BackgroundService.onStartCommand() now uses START_REDELIVER_INTENT
-	 * 17: app version 2.1.4, fixed a bug that still showed the next survey, even if that survey time had been deleted in the backend and the update had propagated to the phone */
+	 * 17: app version 2.1.4, fixed a bug that still showed the next survey, even if that survey time had been deleted in the backend and the update had propagated to the phone
+	 * 18: app version 2.1.5, fixed bugs with recording received SMS messages and sent MMS messages */
 
 	private static String androidID;
 	private static String bluetoothMAC;
