2088b51e59a087fe2eea20a875e21caf39a01764
devsen <arunabhs@andrew.cmu.edu>
2018-04-29 14:15:38 -0400

    Updated the uber fragment

--- app/src/main/java/org/beiwe/app/ui/fragments/UberFragment.java
+++ app/src/main/java/org/beiwe/app/ui/fragments/UberFragment.java
@@ -71,8 +71,7 @@ public class UberFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_uber, container, false);
         //RelativeLayout rlView = (RelativeLayout) v.findViewById(R.id.rlView);
